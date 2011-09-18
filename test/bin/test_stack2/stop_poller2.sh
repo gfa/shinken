958ed163449728730ab05018369813d9ffd0f09e
Arthur Gautier <superbaloo@superbaloo.net>
2011-09-18 12:38:08 +0200

    Removes deprecated files

--- test/bin/test_stack2/stop_poller2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR=$(cd $(dirname "$0"); pwd)
-BIN=$DIR"/../../bin"
-ETC=$DIR"/../../etc/test_stack2"
-
-echo "Stopping poller"
-kill `cat $DIR/../../var/pollerd-2.pid`
--- test/bin/test_stack2/stop_scheduler2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR=$(cd $(dirname "$0"); pwd)
-BIN=$DIR"/../../bin"
-ETC=$DIR"/../../etc/test_stack2"
-
-echo "Stopping scheduler"
-kill `cat $DIR/../../var/schedulerd-2.pid`
